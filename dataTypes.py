4388cca12365ac9fe57d7e2c8612ee4e54148a90
Adam Cox <adamcox.dev@gmail.com>
2023-02-15 22:32:39 -0700

    Saving progress- a lot of reading. Added notes.

--- dataTypes.py
+++ dataTypes.py
@@ -1,1 +1,7 @@
+#  ------------- Data Types Notes and Reference Code ------------
 
+# Entity, Observation, Instance are all interchangeable terms
+# Variable types: Numerical, Categorical
+# Nomial Variavble = named value
+# Dichotmomous Variable = varibale on has two options ex: on/off, yes/no, 0/1, true/false
+# Ordinal variable = ordered variables ex: 1-5
