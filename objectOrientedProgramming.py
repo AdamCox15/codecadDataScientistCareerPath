6cff8ae9dfaaf2b80e6b120245715fe65a8e4a55
Adam Cox <adamcox.dev@gmail.com>
2023-02-16 19:05:15 -0700

    Saving

---
+++ objectOrientedProgramming.py
@@ -0,0 +1,15 @@
+#  ------------ Object-Oriented Programming ----------
+
+class Employee:
+  new_id = 1
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+  def say_id(self):
+    print("My id is {}.".format(self.id))
+e1 = Employee()
+e1.say_id()
+e2 = Employee()
+e2.say_id()
+
+
